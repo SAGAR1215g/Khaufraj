b4d9f6211e42b8d0f9df5b314e8cf6d42904757d
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-25 15:54:54 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -25,7 +25,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SAGAR1215g/IshuXDil"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://te.legra.ph/file/0d873bf61907b7813ff7e.mp4")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tesyyai12345")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/tesyyai12345")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
